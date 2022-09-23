e34077acd712c49da6212c7a0b0b13e5c65ab1a5
Mahmut Can SARIBAL <mahmutcansaribal_19@hotmail.com>
2022-09-23 13:44:21 +0300

    2. Week Homework.

---
+++ Hafta_2_Odevler/Methods.java
@@ -0,0 +1,54 @@
+
+public class Methods {
+
+	public static void main(String[] args) {
+		sayiBulmaca();
+	}
+	
+	public static void sayiBulmaca() {
+		int [] sayilar = new int [] {1,2,5,7,9,0};
+		int aranacak = 3;
+		boolean varMi = false;
+		
+		for(int i = 0; i < sayilar.length;i++) {
+			if(aranacak == sayilar[i]) {
+				varMi = true;
+				break;
+			}
+		}
+		
+		/*
+		 	if(varMi) {
+			mesajver("Sayı mevcuttur : "+aranacak);
+		}else {
+			mesajver("Sayı mevcut degildir : "+aranacak);
+		}
+		 	
+		 */
+		
+		
+		/*	Methodumuza aranan sayıyı ve varMi durumunu gonderdik
+		  	varMi durumuna gore gelecek mesaj methoda iletildi.
+		 */
+		mesajVer(aranacak, varMi);
+	}
+	
+	public static void mesajVer(int aranacak, boolean varMi) {
+		if(varMi) {
+			System.out.println("Aranan sayi dizide var! -> "+aranacak);
+		}else {
+			System.out.println("Aranan sayi dizide yok! -> "+aranacak);
+		}
+		
+	}
+	
+	/*
+		
+		public static void mesajVer(String mesaj) {
+			
+		}
+		
+	 */
+	
+
+}
