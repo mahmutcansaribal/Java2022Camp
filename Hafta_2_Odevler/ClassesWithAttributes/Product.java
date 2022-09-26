e2f0479c8761c12a317a0e479436475a8ee793f9
Mahmut Can SARIBAL <mahmutcansaribal_19@hotmail.com>
2022-09-26 14:27:28 +0300

    Field and Attribute

---
+++ Hafta_2_Odevler/ClassesWithAttributes/Product.java
@@ -0,0 +1,10 @@
+package ClassesWithAttributes;
+
+public class Product {
+	//Attribute | Field
+	int id;
+	String name;
+	String description;
+	double price;
+	int stockAmount;
+}
---
+++ Hafta_2_Odevler/ClassesWithAttributes/ProductManager.java
@@ -0,0 +1,9 @@
+package ClassesWithAttributes;
+
+public class ProductManager {
+
+	public void Add(Product product) {
+		
+		System.out.println("Urun Eklendi : "+product.name);
+	}
+}
