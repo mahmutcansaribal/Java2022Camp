b9914f0b5ca470bb9457012221575c4536da0205
Mahmut Can SARIBAL <mahmutcansaribal_19@hotmail.com>
2022-10-11 15:45:34 +0300

    Interface Demo

---
+++ Hafta_3_Odevler/InterfaceDemo/Main.java
@@ -0,0 +1,10 @@
+package InterfaceDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Hafta_3_Odevler/InterfaceDemo/Robot.java
@@ -0,0 +1,11 @@
+package InterfaceDemo;
+
+public class Robot implements IWorkable {
+
+	@Override
+	public void work() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
